5fbf4644e12452d46f94dbb78f67a803ff945917
UN1X00 <un1x00@mail.com>
2018-05-31 07:47:17 +0100

    Update dirty.c

--- dirty.c
+++ dirty.c
@@ -1,17 +1,4 @@
-//
-// This exploit uses the pokemon exploit of the dirtycow vulnerability
-// as a base and automatically generates a new passwd line.
-// The user will be prompted for the new password when the binary is run.
-// The original /etc/passwd file is then backed up to /tmp/passwd.bak
-// and overwrites the root account with the generated line.
-// After running the exploit you should be able to login with the newly
-// created user.
-//
-// To use this exploit modify the user values according to your needs.
-//   The default is "firefart".
-//
-// Original exploit (dirtycow's ptrace_pokedata "pokemon" method):
-//   https://github.com/dirtycow/dirtycow.github.io/blob/master/pokemon.c
+
 //
 // Compile with:
 //   gcc -pthread dirty.c -o dirty -lcrypt
@@ -19,14 +6,10 @@
 // Then run the newly create binary by either doing:
 //   "./dirty" or "./dirty my-new-password"
 //
-// Afterwards, you can either "su firefart" or "ssh firefart@..."
+// Afterwards, you can either "su root" or "ssh root@..."
 //
 // DON'T FORGET TO RESTORE YOUR /etc/passwd AFTER RUNNING THE EXPLOIT!
 //   mv /tmp/passwd.bak /etc/passwd
-//
-// Exploit adopted by Christian "FireFart" Mehlmauer
-// https://firefart.at
-//
 
 #include <fcntl.h>
 #include <pthread.h>
